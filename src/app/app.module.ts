73b4d8956ff68e4624d6f8b8d4ba211d4d4368d0
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-03-04 20:04:03 +0100

    style du header et creation de theme tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LucideAngularModule, ShoppingBasket, UserCircle} from 'lucide-angular';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    LucideAngularModule.pick({ ShoppingBasket, UserCircle})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
