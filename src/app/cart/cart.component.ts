cb95eaddea7c787e197bb7abfc74ca34a5a1fd7b
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-03-18 19:13:13 +0100

    séparation box et drink dans cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,101 +4,73 @@ import { OnInit } from '@angular/core';
 import { Box } from '../../model/box';
 import { Drink } from '../../model/drink';
 
-
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
 })
 export class CartComponent implements OnInit {
-  public item!: any;
+  public boxs!: Array<any>; 
+  public drinks!: Array<any>; 
   constructor(private LocalService: LocalService) {}
-
   ngOnInit() {
     this.findAllItems();
   }
-
   private findAllItems(): void {
-    const boxs: any[] = this.LocalService.GetBox();
-    const drinks: any[] = this.LocalService.GetDrink();
-    this.item = [...boxs, ...drinks];
+    this.boxs = this.LocalService.GetBox();
+    this.drinks = this.LocalService.GetDrink();
   }
-  
-  public addItemsToCard(id: number): void {
-    this.addBoxOrDrinkToCard(id, 'box');
-    this.addBoxOrDrinkToCard(id, 'drink');
-  }
-  
-  private addBoxOrDrinkToCard(id: number, itemType: string): void {
-    if (itemType === 'box') {
-      const boxs: any[] = this.LocalService.GetBox();
-      const index: number = boxs.findIndex((element: Box) => element.id === id);
-      if (index !== -1) {
-        boxs[index].quantity++;
-        boxs[index].price += boxs[index].startprice;
-        this.LocalService.SetBoxs(boxs);
-      }
-    } else if (itemType === 'drink') {
-      const drinks: any[] = this.LocalService.GetDrink();
-      const index: number = drinks.findIndex((element: Drink) => element.id === id);
-      if (index !== -1) {
-        drinks[index].quantity++;
-        drinks[index].price += drinks[index].startprice;
-        this.LocalService.SetDrinks(drinks);
-      }
-      this.findAllItems();
+  public addBoxToCard (id: number)  : void  {
+    const index: number = this.boxs.findIndex((element: Box) => element.id === id);
+    if (index !== -1) {
+      this.boxs[index].quantity++;
+      this.boxs[index].price += this.boxs[index].startprice;
+      this.LocalService.SetBoxs(this.boxs);
     }
-
   }
-  
-
-  
-
-  public removeItemFromCard(id: number): void {
-    this.removeBoxOrDrinkFromCard(id, 'box');
-    this.removeBoxOrDrinkFromCard(id, 'drink');
-  }
-  
-  private removeBoxOrDrinkFromCard(id: number, itemType: string): void {
-    if (itemType === 'box') {
-      const boxs: any = this.LocalService.GetBox();
-      const index: number = boxs.findIndex((element: Box) => element.id === id);
-      if (index !== -1 && boxs[index].quantity > 0) {
-        boxs[index].quantity--;
-        boxs[index].price -= boxs[index].startprice;
-        this.LocalService.SetBoxs(boxs);
-      }
-    } else if (itemType === 'drink') {
-      const drinks: any = this.LocalService.GetDrink();
-      const index: number = drinks.findIndex((element: Drink) => element.id === id);
-      if (index !== -1 && drinks[index].quantity > 0) {
-        drinks[index].quantity--;
-        drinks[index].price -= drinks[index].startprice;
-        this.LocalService.SetDrinks(drinks);
-      }
+  public addDrinkToCard (id: number)  : void  {
+    const index: number = this.drinks.findIndex((element: Drink) => element.id === id);
+    if (index !== -1) {
+      this.drinks[index].quantity++;
+      this.drinks[index].price += this.drinks[index].startprice;
+      this.LocalService.SetDrinks(this.drinks);
     }
+  }
+  public removeBoxToCard (id: number) : void {
+    const index: number = this.boxs.findIndex((element: Box) => element.id === id);
+    if (index !== -1 && this.boxs[index].quantity > 0) {
+      this.boxs[index].quantity--;
+      this.boxs[index].price -= this.boxs[index].startprice;
+      this.LocalService.SetBoxs(this.boxs);
+    }
+  }
+  public removeDrinkToCard (id: number)  : void {
+    const index: number = this.drinks.findIndex((element: Drink) => element.id === id);
+    if (index !== -1 && this.drinks[index].quantity > 0) {
+      this.drinks[index].quantity--;
+      this.drinks[index].price -= this.drinks[index].startprice;
+      this.LocalService.SetDrinks(this.drinks);
+    }
+  }
+  public deleteBox(id: number) : void  {
+    this.LocalService.clearCartBox(id);
+    this.findAllItems();
+  }
+  public deleteDrink(id: number) : void  {
+    this.LocalService.clearCartDrink(id);
     this.findAllItems();
   }
 
+/*
   public totalCart(): number {
     let total = 0;
-    this.item.forEach((item: Box | Drink) => {
+    item : 
+    item.forEach((item: Box | Drink) => {
       total += +item.quantity * +item.startprice;
     });
-    
+
     return total;
-    
   }
-
-  public deleteItem(id : number){
-this.LocalService.clearCartBox(id);
-this.LocalService.clearCartDrink(id);
-this.findAllItems();
-  }
-
-  public deleteDrink(){
-
-  }
-  
-  
+*/ 
+ 
 }
+
